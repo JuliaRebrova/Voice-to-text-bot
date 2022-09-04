26947ef5b829ceb8f2475bf8f73c9383bfd75290
JuliaRebrova <juliarebrova1@gmail.com>
2022-09-04 16:40:33 +0300

    heroku version

--- main.py
+++ main.py
@@ -2,19 +2,37 @@
 import logging 
 import os
 from aiogram import Bot, Dispatcher, executor, types
-from token_bot import tok
-
+from aiogram.utils.executor import start_webhook
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 from speech import recognize
 
+
 # Configure logging
 logging.basicConfig(level=logging.INFO)
-          
 
-bot = Bot(token=tok)
-dp = Dispatcher(bot) 
+TOKEN = os.getenv('BOT_TOKEN')
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
 
+HEROKU_APP_NAME = os.getenv('HEROKU_APP_NAME')
+
+# webhook settings
+WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
+WEBHOOK_PATH = f'/webhook/{TOKEN}'
+WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
+
+# webserver settings
+WEBAPP_HOST = '0.0.0.0'
+WEBAPP_PORT = os.getenv('PORT', default=8000)
+
+
+async def on_startup(dispatcher):
+    await bot.set_webhook(WEBHOOK_URL, drop_pending_updates=True)
+
+
+async def on_shutdown(dispatcher):
+    await bot.delete_webhook()
 
 button_start = KeyboardButton('/start')
 start_kb = ReplyKeyboardMarkup()
@@ -48,5 +66,13 @@ async def download(ms: types.Message):
 
 
 if __name__ == '__main__':
-    executor.start_polling(dp,skip_updates=True)
-
+    logging.basicConfig(level=logging.INFO)
+    start_webhook(
+        dispatcher=dp,
+        webhook_path=WEBHOOK_PATH,
+        skip_updates=True,
+        on_startup=on_startup,
+        on_shutdown=on_shutdown,
+        host=WEBAPP_HOST,
+        port=WEBAPP_PORT,
+    )
